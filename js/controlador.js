ecf9abb2be88de6a3af4a0aec1b9f4ed662a4bc5
JavierAmaya <photojavhn@gmail.com>
2021-06-16 13:16:51 -0600

    CRUD

--- js/controlador.js
+++ js/controlador.js
@@ -494,8 +494,325 @@ var categorias = [
 console.log ('Usuarios', usuarios);
 console.log ('Categorias', categorias);
 
-const modalEmpresa = () => {
+// local storage
+
+var localStorage = window.localStorage;
+
+
+function setLocalStorage() {
+    /// JSON Categorias
+    if (localStorage.getItem('categorias') == null) {
+        localStorage.setItem('categorias',JSON.stringify(categorias));
+    } else {
+       
+        console.log('local storage full');
+    }
+
+    ///JSON Usuarios
+
+    if (localStorage.getItem('usuarios')==null) {
+        localStorage.setItem('usuarios', JSON.stringify(usuarios));
+    }else {
+        //localStorage.clear();
+        console.log('local Storage full');
+    }
+
+    console.log('LocalStorage Updated');
+}
+
+setLocalStorage();
+
+const setViewNewCat = () =>{
+
+    document.getElementById('empresasViews').innerHTML += `
+            <div class="col-xl-3 col-md-6 col-sm-12" onclick='showModalNewCat()'>
+                <div style="background-color:#45e358;" class="card borderCards">
+                    <div class="icono" alt="icono" ><i class="fas fa-plus"></i></div>
+                    <div class="card-body">
+                    <h5 class="card-title titleCard">Nueva Categoria</h5>
+                    <p class="card-text titleComercioCard"> Presione aqui para crear una nueva categoria</p>
+                    </div>
+                </div>
+            </div>
+    `; 
+
+};
+
+setViewNewCat();
+
+const setCategorias = () => {
+
+    let dataLocalStorage = JSON.parse(localStorage.getItem('categorias'));
+    document.getElementById('empresasViews').innerHTML= '';
+
+    let mensaje = '0 Categorias';
+
+    dataLocalStorage.forEach((cats,index) => {
+        document.getElementById('empresasViews').innerHTML += `
+            <div class="col-xl-3 col-md-6 col-sm-12" onclick='modalCategoria(${index})'>
+                <div style="background-color:${cats.color};" class="card borderCards">
+                    <div class="icono" alt="icono" ><i class="${cats.icono}"></i></div>
+                    <div class="card-body">
+                    <h5 class="card-title titleCard">${cats.nombreCategoria}</h5>
+                    <h5 class="card-text id="cantidadComercios" titleComercioCard"> ${cats.empresas.length} Comercios</h5>
+                    </div>
+                </div>
+            </div>
+        `; 
+    });
+
+
+    setViewNewCat();
+}
+
+setCategorias();
+
+const showModalNewCat = () =>{
+    $('#modNewCat').modal('show');
+}
+
+const setNewCat = () => {
+
+    const dataNewCat = {
+        "nombreCategoria" : document.getElementById('nameCategoria').value,
+        "color" : document.getElementById('colorCategoria').value,
+        "icono" :document.getElementById('iconoCategoria').value,
+        "descripcion": document.getElementById('desccripCategoria').value,
+        "empresas": [
+            {
+                "imagen" : '',
+                "nombreEmpresa": "",
+                "productos": [{}]
+            }
+        ]
+    };
+
+    let catsLocalS = JSON.parse(localStorage.getItem('categorias'));
+    console.log(catsLocalS);
+    catsLocalS.push(dataNewCat);
+    console.log(catsLocalS);
+
+    localStorage.setItem('categorias',JSON.stringify(catsLocalS));
+    alert('Categoria agregada con Exito');
+    //location.reload();
+
+    $('#modNewCat').modal('hide');
+    
+    setCategorias();
+
+};
+
+
+const modalCategoria = (indiceCategoria) => {
     $('#modEmpresas').modal('show');
 
+    let dataLocalStorage = JSON.parse(localStorage.getItem('categorias'));
+
+    let i ;
+
+    document.getElementById('catSelected').innerHTML = `${dataLocalStorage[indiceCategoria].nombreCategoria}`;
+    document.getElementById('divEmpresas').innerHTML = '';
+
+    dataLocalStorage[indiceCategoria].empresas.forEach((categs,i) => {
+        console.log('empresas', categs);
+
+        console.log('esto trae',categs.nombreEmpresa);
+        
+        if (categs.nombreEmpresa == "") {
+            console.log('entro al if');
+            document.getElementById('divEmpresas').innerHTML = `
+                <h5 style="background-color: red" > Esta categoria aun no tiene empresas </h5>
+            `;
+            
+        } else {
+        
+            document.getElementById('divEmpresas').innerHTML += `
+                <div class="col-xl-6 col-md-6 col-sm-12 mgCardsModal">
+                    <div class="card">
+                        <div>
+                            <img class="card-img-top titleCardModal" src="./${categs.imagen}" alt="Card image cap">
+                            <h5 class="titleEmpresa">${categs.nombreEmpresa}</h5>
+                        </div>
+                        <ul id="listaProductos${i}" class="list-group list-group-flush">
+
+                        </ul>
+                    </div>
+                </div>
+            `;
+
+            categs.productos.forEach((element,j) => {
+                document.getElementById('listaProductos'+i).innerHTML +=`
+                    <li class="list-group-item">
+                        <div class="row">
+                            <div class="col-6"> 
+                                <h5><strong class="titleProductModal">${element.nombreProducto}</strong></h5>
+                            </div>
+                            <div class="col-6">
+                                <button class="btn btn-primary btnPedir" onclick='newOrder(${indiceCategoria},${i},${j})' >Pedir</button>
+                            </div>                                                
+                        </div>
+                        <div class="row">
+                            <div class="col-12 mCometario">
+                                <p>${element.descripcion}</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <h6 class="alingPrice">$${element.precio}</h6>
+                            </div>
+                        </div>
+                    </li>
+
+                `;
+            });
+
+        }
+    });
+} /// llave function
+
+
+const setSelectUsers = () => {
+
+    let dataLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
+    
+    document.getElementById('selectUsers').innerHTML = '';
+    document.getElementById('selectUsers').innerHTML = `
+
+      <option selected value=-1>Select User</option>
+
+    `;
+    
+    dataLocalStorage.forEach((user, index) => {
+        document.getElementById('selectUsers').innerHTML += `
+            <option value="${index}">${user.nombre} ${user.apellido}</option>
+        `;
+    });
 }
+
+
+
+setSelectUsers();
+
+
+const usuarioSelected = () => {
+    
+
+    let dataLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
+    let user = document.getElementById('selectUsers').value;
+
+    console.log(user);
+
+    let usuarioLogueado = [{
+        "user" : user
+    }];
+
+    if (user !=-1) {
+        document.getElementById('userLogged').innerHTML = `${dataLocalStorage[user].nombre}!`;
+        localStorage.setItem('userLogin',JSON.stringify(usuarioLogueado));
+    } else {
+        location.reload();
+    }
+};
+
+const infoModalOrders = () => {
+    let dataLocalStorage = JSON.parse(localStorage.getItem('usuarios'));
+    let user = document.getElementById('selectUsers').value;
+
+    console.log(user);
+
+    document.getElementById('ulListOrders').innerHTML = '';
+    if (user !=-1) {
+
+        document.getElementById('userLoggedOrders').innerHTML = `${dataLocalStorage[user].nombre}`;
+
+        dataLocalStorage[user].ordenes.forEach(orders => {
+            document.getElementById('ulListOrders').innerHTML += `
+                <li class="list-group-item">
+                    <h5 class="colorBase">${orders.nombreProducto}</h5>
+                    <p>${orders.descripcion}</p>
+                    <h5 class="alingPrice">$${orders.precio}</h5>
+                </li>
+            `; 
+        });
+
+    }else {
+        location.reload();
+    }
+};
+
+
+const newOrder = (iCat, iEmpresa, iProduct) => {
+
+
+    $('#modalPedirProducto').modal('show');
+
+    let dataLocalStorage = JSON.parse(localStorage.getItem('categorias'));
+
+    let ubicacionPedido = [{
+        "idCat":iCat,
+        "idEmpresa":iEmpresa,
+        "idProducto":iProduct
+    }];
+
+    localStorage.setItem('ubicacionPedidoNuevo',JSON.stringify(ubicacionPedido));
+
+    document.getElementById('nameProductOrder').innerHTML = `${dataLocalStorage[iCat].empresas[iEmpresa].productos[iProduct].nombreProducto}`;
+    document.getElementById('nameProductOrder').value = `${dataLocalStorage[iCat].empresas[iEmpresa].productos[iProduct].nombreProducto}`;
+    
+    
+    document.getElementById('descripProductOrder').innerHTML = `${dataLocalStorage[iCat].empresas[iEmpresa].productos[iProduct].descripcion}`;
+    document.getElementById('descripProductOrder').value = `${dataLocalStorage[iCat].empresas[iEmpresa].productos[iProduct].descripcion}`;
+    
+    document.getElementById('priceProductOrder').innerHTML = `$${dataLocalStorage[iCat].empresas[iEmpresa].productos[iProduct].precio}`;
+    document.getElementById('priceProductOrder').value = `${dataLocalStorage[iCat].empresas[iEmpresa].productos[iProduct].precio}`;
+
+};
+
+const procesarOrder = () => {
+
+    const order = {
+        "nombreProducto" :  document.getElementById('nameProductOrder').value,
+        "descripcion" :  document.getElementById('descripProductOrder').value,
+        "precio" : document.getElementById('priceProductOrder').value,
+        "cantidad" : document.getElementById('cantidadOrder').value
+    };
+
+    let ubicacion = JSON.parse(localStorage.getItem('ubicacionPedidoNuevo'));
+
+    /*ubicacion.forEach(element => {
+        console.log(element.idCat);
+        console.log(element.idEmpresa);
+        console.log(element.idProducto);        
+    });*/
+
+    let usuarioLogueado = JSON.parse(localStorage.getItem('userLogin'));
+    //console.log('usuario en sesion', usuarioLogueado[0].user);
+
+    if ( usuarioLogueado == null ) {
+        alert('Por favor inicie sesion con un usuario valido');
+        $('#modalPedirProducto').modal('hide');
+        $('#modEmpresas').modal('hide');
+    }else {
+        console.log(order,ubicacion);
+
+        // insertado el registro
+        
+        let usersLocalS = JSON.parse(localStorage.getItem('usuarios'));
+        //usuarios[usuarioLogueado].ordenes.push(order);
+        console.log(usersLocalS[usuarioLogueado[0].user].ordenes);
+    
+        usersLocalS[usuarioLogueado[0].user].ordenes.push(order);
+    
+        localStorage.setItem('usuarios',JSON.stringify(usersLocalS));
+    
+        console.log(usersLocalS[usuarioLogueado[0].user].ordenes);
+        alert('Orden Procesada con Exito');
+        //location.reload();
+
+        $('#modalPedirProducto').modal('hide');
+        $('#modEmpresas').modal('hide');
+    }
+
+}
+
 
